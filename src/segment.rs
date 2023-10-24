9b26b6245e036ed40bbbed5c5d302dfd6bf15720
Paul Kutrich <pkutrich@gmail.com>
2023-10-24 13:01:12 -0700

    correct types. use unwrap.

--- src/segment.rs
+++ src/segment.rs
@@ -33,34 +33,34 @@ impl Segment {
             } else {
                 val = val & (1 << (7 - i));
             }
-            self.dio.set_value(val)?;
-            self.clk.set_value(true)?;
+            self.dio.set_value(val).unwrap();
+            self.clk.set_value(true).unwrap();
             thread::sleep(Duration::from_millis(10));
-            self.clk.set_value(false)?;
+            self.clk.set_value(false).unwrap();
             thread::sleep(Duration::from_millis(10));
         }
     }
 
     fn send_command(mut self, cmd: u8) {
-        self.stb.set_value(false)?;
+        self.stb.set_value(false).unwrap();
         self.shift_out(cmd);
-        self.stb.set_value(true)?;
+        self.stb.set_value(true).unwrap();
     }
 
     pub fn display_num(mut self, num: u8) {
         let digits: Vec<i8> = vec![0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f];
         self.send_command(0x40);
-        self.stb.set_value(false)?;
+        self.stb.set_value(false).unwrap();
         self.shift_out(0xc0);
-        self.shift_out(digits[(num as u16/1000)%10]);
+        self.shift_out(digits[((num as u16 / 1000) % 10) as usize] as u8);
         self.shift_out(0x00);
-        self.shift_out(digits[(num/100)%10]);
+        self.shift_out(digits[((num/100)%10) as usize] as u8);
         self.shift_out(0x00);
-        self.shift_out(digits[(num/10)%10]);
+        self.shift_out(digits[((num/10)%10) as usize] as u8);
         self.shift_out(0x00);
-        self.shift_out(digits[num%10]);
+        self.shift_out(digits[(num%10) as usize] as u8);
         self.shift_out(0x00);
-        self.stb.set_value(true)?;
+        self.stb.set_value(true).unwrap();
     }
 
     pub fn display_dec(mut self, num: f32) {
@@ -68,22 +68,22 @@ impl Segment {
         let integer: i32;
         let decimal: i32;
 
-        let pro: i32 = (num * 100);
+        let pro: f32 = num * 100.0;
 
         integer = pro / 100;
         decimal = pro % 100;
         self.send_command(0x40);
-        self.stb.set_value(false)?;
+        self.stb.set_value(false).unwrap();
         self.shift_out(0xc0);
-        self.shift_out(digits[integer as u16/10]);
+        self.shift_out(digits[(integer as u16/10) as usize]);
         self.shift_out(0x00);
-        self.shift_out(digits[(integer%10) | 0x80]);
+        self.shift_out(digits[((integer%10) | 0x80) as usize]);
         self.shift_out(0x00);
-        self.shift_out(digits[decimal/10]);
+        self.shift_out(digits[(decimal/10) as usize]);
         self.shift_out(0x00);
-        self.shift_out(digits[decimal%10]);
+        self.shift_out(digits[(decimal%10) as usize]);
         self.shift_out(0x00);
-        self.stb.set_value(true)?;
+        self.stb.set_value(true).unwrap();
     }
 }
 
