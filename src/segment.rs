bcaa7553bd1d6932dc413950a5d5215844a1420a
Paul Kutrich <pkutrich@gmail.com>
2023-10-24 13:35:29 -0700

    pad dec with zero, try msbf.

--- src/segment.rs
+++ src/segment.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 const DIO: u16 = 27;
 const CLK: u16 = 18;
 const STB: u16 = 17;
-const BIT_ORDER: u8 = 1;
+const BIT_ORDER: u8 = 0;
 
 pub struct Segment {
     dio: gpio::sysfs::SysFsGpioOutput,
@@ -72,7 +72,7 @@ impl Segment {
         let str_num = num.to_string();
 
         integer = i32::from_str(&str_num[..2]).unwrap();
-        decimal = i32::from_str(&str_num[3..=4]).unwrap();
+        decimal = i32::from_str(&str_num[3..=4]).unwrap() + "0";
         self.send_command(0x40);
         self.stb.set_value(false).unwrap();
         self.shift_out(0xc0);
