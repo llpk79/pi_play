588bd59f118a308b6a14c58920378ac69206428b
Paul Kutrich <pkutrich@gmail.com>
2023-10-24 16:44:16 -0700

    add fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::{thread};
 use std::time::Duration;
 use std::fs::read_to_string;
-// use std::str::FromStr;
+use std::str::FromStr;
 use pi_play_lib::segment::{Segment};
 
-fn read_temp() -> String {
+fn read_temp(f: bool) -> String {
     let file_str: String = read_to_string("/sys/bus/w1/devices/28-3ce1d443e7e1/w1_slave").unwrap().parse().unwrap();
     println!("file_str {}", file_str);
-    let temp_str: String = file_str[69..74].to_string() + "0";
-    // let mut temp = f32::from_str(&temp_str).unwrap();
-    // temp /= 1000.0;
+    let mut temp_str: String = file_str[69..74].to_string();
+    if f {
+        let mut temp = f32::from_str(&temp_str).unwrap();
+        temp /= 1000.0;
+        temp = (temp * (9/5)) + 32;
+        temp_str = temp.to_string()
+    }
     temp_str
 }
 
@@ -18,7 +22,7 @@ fn main() {
     segment_display.init();
     print!("segment {:?}", segment_display);
     loop {
-        let temp = read_temp();
+        let temp = read_temp(true);
         segment_display.display_dec(temp.clone());
         println!("Current temp: {} C", temp);
         thread::sleep(Duration::from_millis(1000));
