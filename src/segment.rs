42208d7aae3e9ded1377482d1a34986b7024585e
Paul Kutrich <pkutrich@gmail.com>
2023-10-24 20:45:22 -0700

    adjust error message.

--- src/segment.rs
+++ src/segment.rs
@@ -87,13 +87,13 @@ impl Segment {
         self.send_command(0x40);
         self.stb.set_value(false).unwrap();
         self.shift_out(0xc0);
-        self.shift_out(0xd9);
+        self.shift_out(0xf9);
         self.shift_out(0x00);
-        self.shift_out(0xd9);
+        self.shift_out(0xf9);
         self.shift_out(0x00);
-        self.shift_out(0xd9);
+        self.shift_out(0xf9);
         self.shift_out(0x00);
-        self.shift_out(0xd9);
+        self.shift_out(0xf9);
         self.shift_out(0x00);
         self.stb.set_value(true).unwrap();
     }
