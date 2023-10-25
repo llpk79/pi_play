9a675c7348a806b388104a7e053f324712e4cb9b
Paul Kutrich <pkutrich@gmail.com>
2023-10-24 18:47:12 -0700

    read_temp -> temp.rs. import motor.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,25 @@
 use std::{thread};
 use std::time::Duration;
-use std::fs::read_to_string;
-use std::str::FromStr;
 use pi_play_lib::segment::{Segment};
+use pi_play_lib::temp::{read_temp};
+use pi_play_lib::motor::{Motor};
+use std::str::FromStr;
 
 const FAHRENHEIT: bool = false;
-
-fn read_temp(fahrenheit: bool) -> String {
-    let file_str: String = read_to_string("/sys/bus/w1/devices/28-3ce1d443e7e1/w1_slave").unwrap().parse().unwrap();
-    let mut temp_str: String = file_str[69..74].to_string();
-    if fahrenheit {
-        let mut temp = f32::from_str(&temp_str).unwrap();
-        temp /= 1000.0;
-        temp = (temp * 9.0/5.0) + 32.0;
-        temp_str = temp.to_string().replace(".", "") + "00";
-    }
-    temp_str
-}
 
 fn main() {
     let mut segment_display = Segment::new();
     segment_display.init();
+    let mut motor = Motor::new();
+    motor.stop_motor();
     loop {
         let mut temp = read_temp(FAHRENHEIT);
+        let motor_on: bool = i32::from_str(&temp).unwrap() > 1300;
+        if motor_on {
+            motor.start_motor();
+        } else {
+            motor.stop_motor();
+        }
         segment_display.display_dec(temp.clone());
         temp.insert(2, '.');
         temp = temp[..5].to_string();
