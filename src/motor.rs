5fa8df05fb607969dd5e4ef03102eb67ed95d073
Paul Kutrich <pkutrich@gmail.com>
2023-10-24 18:47:23 -0700

    initial commit.

---
+++ src/motor.rs
@@ -0,0 +1,24 @@
+use gpio::{GpioOut};
+
+pub struct Motor {
+    in_: gpio::sysfs::SysFsGpioOutput,
+    out: gpio::sysfs::SysFsGpioOutput
+}
+
+impl Motor {
+    pub fn new() -> Motor {
+        let in_ = gpio::sysfs::SysFsGpioOutput::open(5).unwrap();
+        let out = gpio::sysfs::SysFsGpioOutput::open(6).unwrap();
+        Self {in_, out}
+    }
+
+    pub fn start_motor(mut self) {
+        self.out.set_value(true).unwrap();
+        self.in_.set_value(false).unwrap();
+    }
+
+    pub fn stop_motor(mut self) {
+        self.out.set_value(true).unwrap();
+        self.in_.set_value(true).unwrap();
+    }
+}
---
+++ src/temp.rs
@@ -0,0 +1,15 @@
+use std::fs::read_to_string;
+use std::str::FromStr;
+
+pub fn read_temp(fahrenheit: bool) -> String {
+    let file_str: String = read_to_string("/sys/bus/w1/devices/28-3ce1d443e7e1/w1_slave").unwrap().parse().unwrap();
+    let mut temp_str: String = file_str[69..74].to_string();
+    if fahrenheit {
+        let mut temp = f32::from_str(&temp_str).unwrap();
+        temp /= 1000.0;
+        temp = (temp * 9.0/5.0) + 32.0;
+        temp_str = temp.to_string().replace(".", "") + "00";
+    }
+    temp_str
+}
+
