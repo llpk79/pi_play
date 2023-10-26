24a64cffc138a71f93d26f8635ba41fd88058339
Paul Kutrich <pkutrich@gmail.com>
2023-10-26 11:06:39 -0700

    correct path

--- src/lcd.rs
+++ src/lcd.rs
@@ -2,7 +2,7 @@ extern crate i2c_linux;
 use i2c_linux::I2c;
 
 pub fn read () {
-    let mut i2c = I2c::from_path("dev/i2c-1").unwrap();
+    let mut i2c = I2c::from_path("/dev/i2c-1").unwrap();
     i2c.smbus_set_slave_address(0x50, false).unwrap();
     let data = i2c.smbus_read_byte().unwrap();
     println!("I2c data: {}", data);
