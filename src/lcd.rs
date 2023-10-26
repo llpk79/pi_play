8316c85ce682af886822fe3841a904c348e82ff7
Paul Kutrich <pkutrich@gmail.com>
2023-10-26 11:09:49 -0700

    check functionality

--- src/lcd.rs
+++ src/lcd.rs
@@ -3,7 +3,7 @@ use i2c_linux::I2c;
 
 pub fn read () {
     let mut i2c = I2c::from_path("/dev/i2c-1").unwrap();
-    i2c.smbus_set_slave_address(0x50, false).unwrap();
-    let data = i2c.smbus_read_byte().unwrap();
-    println!("I2c data: {}", data);
+    // i2c.smbus_set_slave_address(0x50, false).unwrap();
+    let data = i2c.i2c_functionality().unwrap();
+    println!("I2c data: {:?}", data);
 }
