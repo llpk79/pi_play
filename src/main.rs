57388e536f3193ef57713fc7cf124c1e8e0daba2
Paul Kutrich <pkutrich@gmail.com>
2023-10-28 08:45:55 -0700

    add temp_humid

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,6 @@ fn main() {
         // lcd.cursor_to(1, 11);
         // lcd.print_line(&c_temp);
         lcd.display_data(measure_temp_humid());
-        thread::sleep(Duration::from_millis(2000));
+        thread::sleep(Duration::from_millis(3000));
     }
 }
