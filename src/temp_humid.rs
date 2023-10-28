f207f4defdf86f6de3975b9adedeef1ff450b214
Paul Kutrich <pkutrich@gmail.com>
2023-10-28 16:44:29 -0700

    add temp_humid

--- src/temp_humid.rs
+++ src/temp_humid.rs
@@ -10,7 +10,7 @@ pub fn measure_temp_humid() -> Vec<String> {
     let mut start_pin = gpio::sysfs::SysFsGpioOutput::open(18).unwrap();
     // thread::sleep(Duration::from_secs(1));
     start_pin.set_value(false).unwrap();
-    thread::sleep(Duration::from_micros(300));
+    thread::sleep(Duration::from_millis(20));
     start_pin.set_value(true).unwrap();
     let mut data_pin = gpio::sysfs::SysFsGpioInput::open(18).unwrap();
     while data_pin.read_value().unwrap() == Low {
