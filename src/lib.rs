6ef3a3c042c61107bd96883845649728747fe9be
Paul Kutrich <pkutrich@gmail.com>
2023-10-27 19:46:41 -0700

    add temp_humid

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod lcd;
 pub mod motor;
 pub mod segment;
 pub mod temp;
+pub mod temp_humid;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 // use pi_play_lib::segment::Segment;
-use pi_play_lib::temp::read_temp;
+// use pi_play_lib::temp::read_temp;
 use std::thread;
 use std::time::Duration;
 // use pi_play_lib::distance::Distance;
 use pi_play_lib::lcd::LCD;
+use pi_play_lib::temp_humid::TempHumid;
 
 fn main() {
     let mut lcd = LCD::new();
@@ -11,29 +12,31 @@ fn main() {
     lcd.backlight_off();
     lcd.display_init();
     lcd.backlight_on();
-    let data = vec![
-        "Temperature        ".to_string(),
-        "F:      C:     ".to_string(),
-    ];
-    lcd.display_data(data);
+    // let data = vec![
+    //     "Temperature        ".to_string(),
+    //     "F:      C:     ".to_string(),
+    // ];
+    // lcd.display_data(data);
     // let mut segment_display = Segment::new();
     // segment_display.init();
     // let mut distance = Distance::new();
+    let mut hum_temp = TempHumid::new();
     loop {
-        let mut f_temp = read_temp(true);
-        let mut c_temp = read_temp(false);
+        // let mut f_temp = read_temp(true);
+        // let mut c_temp = read_temp(false);
 
         // let measure = distance.print_measure() + "   ";
         // let data = format!("{}.{}", &measure[0..1], &measure[1..]);
         // segment_display.display_dec(measure);
 
-        f_temp = f_temp[..4].to_string();
-        c_temp = c_temp[..4].to_string();
-
-        lcd.cursor_to(1, 3);
-        lcd.print_line(&f_temp);
-        lcd.cursor_to(1, 11);
-        lcd.print_line(&c_temp);
+        // f_temp = f_temp[..4].to_string();
+        // c_temp = c_temp[..4].to_string();
+        //
+        // lcd.cursor_to(1, 3);
+        // lcd.print_line(&f_temp);
+        // lcd.cursor_to(1, 11);
+        // lcd.print_line(&c_temp);
+        lcd.display_data(hum_temp.string_data());
         thread::sleep(Duration::from_millis(1));
     }
 }
