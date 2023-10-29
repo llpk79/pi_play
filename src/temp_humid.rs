1fc74b388cc98c399356c1ca8a1f31e7b4fa88e9
Paul Kutrich <pkutrich@gmail.com>
2023-10-28 17:27:57 -0700

    add temp_humid

--- src/temp_humid.rs
+++ src/temp_humid.rs
@@ -20,17 +20,18 @@ pub fn measure_temp_humid() -> Vec<String> {
         continue
     };
     for i in 0..40 {
+        // while data_pin.read_value().unwrap() == Low {
+        //     continue
+        // }
+        thread::sleep(Duration::from_micros(50));
         println!("{}", i);
-        while data_pin.read_value().unwrap() == Low {
-            continue
-        }
         let start = chrono::Utc::now();
         while data_pin.read_value().unwrap() == High {
             continue
         }
         let end = chrono::Utc::now();
         let bit_time = end - start;
-        // println!("bit time {:?}", bit_time.num_microseconds().unwrap());
+        println!("bit time {:?}", bit_time.num_microseconds().unwrap());
         if bit_time.num_microseconds().unwrap() > 30 {
             data.push(1);
         } else {
