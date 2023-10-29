21f9adddfaf13758696da5d3e591269035b69f2e
Paul Kutrich <pkutrich@gmail.com>
2023-10-28 17:49:25 -0700

    add temp_humid

--- src/temp_humid.rs
+++ src/temp_humid.rs
@@ -28,13 +28,13 @@ pub fn measure_temp_humid() -> Vec<String> {
         let start = chrono::Utc::now();
         let mut limit = 0;
         while data_pin.read_value().unwrap() == High {
-            if limit > 5 {
-                break
-            } else {
-                limit += 1;
-                continue
-            }
-            // continue
+            // if limit > 5 {
+            //     break
+            // } else {
+            //     limit += 1;
+            //     continue
+            // }
+            continue
         }
         // loop {
         //     if data_pin.read_value().unwrap() == Low {
