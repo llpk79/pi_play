58d6bdc5011b266f70a22f00be47efe2993685df
Paul Kutrich <pkutrich@gmail.com>
2023-11-05 10:20:58 -0800

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -22,11 +22,11 @@ impl Laser {
 
     pub fn send_message(&mut self, message: String) {
         // self.out.set_value(false).unwrap();
-        thread::sleep(Duration::from_micros(1000));
+        thread::sleep(Duration::from_millis(10));
         self.out.set_value(true).unwrap();
-        thread::sleep(Duration::from_micros(1000));
+        thread::sleep(Duration::from_millis(10));
         self.out.set_value(false).unwrap();
-        thread::sleep(Duration::from_micros(500));
+        thread::sleep(Duration::from_millis(5));
         let mut data = Vec::new();
         for char in message.chars() {
             let code = char as i8;
@@ -35,22 +35,22 @@ impl Laser {
                 match bit == 1 {
                     true => {
                         self.out.set_value(true).unwrap();
-                        thread::sleep(Duration::from_micros(500));
+                        thread::sleep(Duration::from_millis(5));
                         self.out.set_value(false).unwrap();
                     }
                     false => {
                         self.out.set_value(true).unwrap();
-                        thread::sleep(Duration::from_micros(200));
+                        thread::sleep(Duration::from_millis(2));
                         self.out.set_value(false).unwrap();
                     }
                 }
             }
-            thread::sleep(Duration::from_micros(500))
+            thread::sleep(Duration::from_millis(10))
         }
         self.out.set_value(true).unwrap();
-        thread::sleep(Duration::from_micros(1500));
+        thread::sleep(Duration::from_millis(15));
         self.out.set_value(false).unwrap();
-        thread::sleep(Duration::from_micros(500));
+        thread::sleep(Duration::from_millis(10));
     }
 }
 
@@ -70,8 +70,8 @@ impl Receiver {
             continue;
         }
         let end = chrono::Utc::now();
-        let initiation_time = (end - begin).num_microseconds().unwrap();
-        if (900 < initiation_time) && (initiation_time < 1100) {
+        let initiation_time = (end - begin).num_milliseconds();
+        if (9 < initiation_time) && (initiation_time < 11) {
             println!("bleeep");
             'outer: loop {
                 while self.in_.read_value().unwrap() == Low {
@@ -82,14 +82,14 @@ impl Receiver {
                     continue;
                 }
                 let end = chrono::Utc::now();
-                let bit_time = (end - start).num_microseconds().unwrap();
+                let bit_time = (end - start).num_milliseconds();
                 println!("bit time {}", bit_time);
                 match bit_time {
                     i64::MIN..=-0_i64 => continue,
-                    1..=300 => data.push(0),
-                    301..=510 => data.push(1),
-                    511..=1500 => continue,
-                    1501.. => break 'outer,
+                    1..=3 => data.push(0),
+                    4..=5 => data.push(1),
+                    6..=11 => continue,
+                    12.. => break 'outer,
                 };
             }
         }
