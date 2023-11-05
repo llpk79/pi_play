045ca9151f5a5fc9344e873c710710bc396a212e
Paul Kutrich <pkutrich@gmail.com>
2023-11-04 21:11:59 -0700

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -31,22 +31,23 @@ impl Laser {
         thread::sleep(Duration::from_millis(10));
             for char in message.chars() {
                 let code = char as i8;
+                println!("char {}\ncode {}\n", char, code);
                 for bit in (0..8).map(|n| (code >> n) & 1) {
                     // println!("bit {}", bit);
                     match bit == 1 {
                         true => {
                             self.out.set_value(true).unwrap();
-                            thread::sleep(Duration::from_micros(50));
+                            thread::sleep(Duration::from_millis(5));
                             self.out.set_value(false).unwrap();
                         }
                         false => {
                             self.out.set_value(true).unwrap();
-                            thread::sleep(Duration::from_micros(25));
+                            thread::sleep(Duration::from_millis(2));
                             self.out.set_value(false).unwrap();
                         }
                     }
                 }
-                thread::sleep(Duration::from_micros(5))
+                thread::sleep(Duration::from_millis(5))
             }
     }
 }
@@ -76,9 +77,9 @@ impl Receiver {
                 continue;
             };
             let end = chrono::Utc::now();
-            let bit_time = (end - start).num_microseconds().unwrap();
+            let bit_time = (end - start).num_milliseconds();
             // println!("bit time {}", bit_time);
-            if bit_time > 35 {
+            if bit_time > 3 {
                 data.push(1);
             } else {
                 data.push(0);
