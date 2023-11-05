07cd8b00425e023a4fc690d0e4066e3e35f77fe3
Paul Kutrich <pkutrich@gmail.com>
2023-11-04 23:51:05 -0700

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -99,7 +99,7 @@ impl Receiver {
         for i in (0..data.len() - 1).step_by(8) {
             let mut code: u32 = 0;
             for j in 0..8 {
-                code += data[i + j] * u32::pow(2, 7 - j as u32);
+                code += data[i + j] * u32::pow(2, j as u32);
                 println!("code {}\nindex {}", code, i + j);
             }
             codes.push(code);
