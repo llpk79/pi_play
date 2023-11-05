c7f3478c7105128891779e82517a05dffced0a95
Paul Kutrich <pkutrich@gmail.com>
2023-11-04 23:56:05 -0700

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -4,7 +4,7 @@ use std::thread;
 use std::time::Duration;
 
 const LASER_PIN: u16 = 18;
-const RECIEVER_PIN: u16 = 23;
+const RECEIVER_PIN: u16 = 23;
 
 pub struct Laser {
     out: gpio::sysfs::SysFsGpioOutput,
@@ -23,14 +23,13 @@ impl Laser {
     pub fn send_message(&mut self, message: String) {
         // self.out.set_value(false).unwrap();
         thread::sleep(Duration::from_millis(20));
-         self.out.set_value(true).unwrap();
+        self.out.set_value(true).unwrap();
         thread::sleep(Duration::from_millis(20));
         self.out.set_value(false).unwrap();
         thread::sleep(Duration::from_millis(10));
-        let mut data =  Vec::new();
+        let mut data = Vec::new();
         for char in message.chars() {
             let code = char as i8;
-            // println!("char {}\ncode {}\n", char, code);
             for bit in (0..8).map(|n| (code >> n) & 1) {
                 data.push(bit);
                 match bit == 1 {
@@ -48,13 +47,12 @@ impl Laser {
             }
             thread::sleep(Duration::from_millis(5))
         }
-        // println!("output \n{:?}", data);
     }
 }
 
 impl Receiver {
     pub fn new() -> Receiver {
-        let in_ = gpio::sysfs::SysFsGpioInput::open(RECIEVER_PIN).unwrap();
+        let in_ = gpio::sysfs::SysFsGpioInput::open(RECEIVER_PIN).unwrap();
         Self { in_ }
     }
 
@@ -69,43 +67,38 @@ impl Receiver {
         }
         let end = chrono::Utc::now();
         if (end - begin).num_milliseconds() > 19 {
-        for _ in 0..96 {
-            while self.in_.read_value().unwrap() == Low {
-                continue;
+            for _ in 0..96 {
+                while self.in_.read_value().unwrap() == Low {
+                    continue;
+                }
+                let start = chrono::Utc::now();
+                while self.in_.read_value().unwrap() == High {
+                    continue;
+                }
+                let end = chrono::Utc::now();
+                let bit_time = (end - start).num_milliseconds();
+                if bit_time > 3 {
+                    data.push(1);
+                } else {
+                    data.push(0);
+                };
             }
-            let start = chrono::Utc::now();
-            while self.in_.read_value().unwrap() == High {
-                continue;
-            }
-            let end = chrono::Utc::now();
-            let bit_time = (end - start).num_milliseconds();
-            // println!("bit time {}", bit_time);
-            if bit_time > 3 {
-                data.push(1);
-            } else {
-                data.push(0);
-            };
-        }
         }
         data
     }
 
     pub fn print_message(&mut self) {
-        println!("getting data");
         let data = self.receive_message();
-        println!("decoding data: \n{:?}", data);
         let mut chars = Vec::new();
         let mut codes = Vec::new();
         for i in (0..data.len() - 1).step_by(8) {
             let mut code: u32 = 0;
             for j in 0..8 {
                 code += data[i + j] * u32::pow(2, j as u32);
-                println!("code {}\nindex {}", code, i + j);
             }
             codes.push(code);
             chars.push(char::from_u32(code))
         }
-        println!("codes {:?}", codes);
         let mut message: String = "".to_string();
         for char in chars {
             match char {
