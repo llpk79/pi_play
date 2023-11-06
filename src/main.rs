ce886932608dddd3f66ce487778244ec794cda85
Paul Kutrich <pkutrich@gmail.com>
2023-11-05 16:23:27 -0800

    add laser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // use pi_play_lib::segment::Segment;
 // use pi_play_lib::temp::read_temp;
 use std::thread;
+use std::time::Duration;
 // use std::time::Duration;
 // use pi_play_lib::distance::Distance;
 // use pi_play_lib::lcd::LCD;
@@ -15,9 +16,11 @@ fn main() {
         laser.send_message(
             "That other message was old and tired. Here's something fresh!!".to_string(),
         );
+        thread::sleep(Duration::from_secs(2))
     });
     let receiver_thread = thread::spawn(move || loop {
         receiver.print_message();
+        thread::sleep(Duration::from_secs(2))
     });
     laser_thread.join().unwrap();
     receiver_thread.join().unwrap();
