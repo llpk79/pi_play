e2eb49dcc74b006229524f3dec32cbd3b7694be3
Paul Kutrich <pkutrich@gmail.com>
2023-11-05 21:59:36 -0800

    add laser

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ fn main() {
 
     let laser_thread = thread::spawn(move || loop {
         laser.send_message(
-            fs::read_to_string("./src/temp_humid.rs").unwrap()
-            // "Hello World ".to_string(),
+            // fs::read_to_string("./src/temp_humid.rs").unwrap()
+            "Hello World ".to_string(),
         );
         thread::sleep(Duration::from_secs(2))
     });
