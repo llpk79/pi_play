06cfccca568c6927e8811d567cdba810aa41ed1d
Paul Kutrich <pkutrich@gmail.com>
2023-11-06 16:32:23 -0800

    add laser

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
         .name("laser".to_string())
         .spawn(move || loop {
             laser.send_message(
-                fs::read_to_string("./src/lasers.rs").unwrap(), // "Hello World ".to_string(),
+                fs::read_to_string("./src/main.rs").unwrap(), // "Hello World ".to_string(),
             );
             thread::sleep(Duration::from_secs(5))
         });
