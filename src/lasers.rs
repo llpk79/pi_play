482c0d2053d4277ac606affe34e84326e639ae10
Paul Kutrich <pkutrich@gmail.com>
2023-11-07 16:43:31 -0800

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -53,16 +53,16 @@ impl Laser {
             match bit == 1 {
                 true => {
                     self.out.set_value(true).expect("Error setting pin");
-                    thread::sleep(Duration::from_micros(750));
+                    thread::sleep(Duration::from_micros(650));
                     self.out.set_value(false).expect("Error setting pin");
                 }
                 false => {
                     self.out.set_value(true).expect("Error setting pin");
-                    thread::sleep(Duration::from_micros(500));
+                    thread::sleep(Duration::from_micros(400));
                     self.out.set_value(false).expect("Error setting pin");
                 }
             }
-            thread::sleep(Duration::from_micros(150))
+            thread::sleep(Duration::from_micros(100))
         }
 
         // Termination sequence.
@@ -111,8 +111,8 @@ impl Receiver {
                 // println!("bit time {}", bit_time);
                 match bit_time {
                     i64::MIN..=-0_i64 => continue,
-                    1..=750 => data.push(0),
-                    751..=1575 => data.push(1),
+                    1..=650 => data.push(0),
+                    651..=1575 => data.push(1),
                     1576.. => break 'outer, // Termination sequence.
                 };
             }
