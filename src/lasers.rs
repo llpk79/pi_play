f279b5c9531e8bdcf2c8007bde77d6d361589f64
Paul Kutrich <pkutrich@gmail.com>
2023-11-08 09:45:40 -0800

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -112,7 +112,7 @@ impl Receiver {
             }
             let end = chrono::Utc::now();
             let bit_time = (end - start).num_microseconds().expect("micro");
-            // println!("bit time {}", bit_time);
+            println!("bit time {}", bit_time);
             match bit_time {
                 i64::MIN..=-0_i64 => continue,
                 1..=250 => data.push(0),
