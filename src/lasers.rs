7d9e64e31546359ae3484865a945f8adcae56ba1
Paul Kutrich <pkutrich@gmail.com>
2023-11-08 11:42:35 -0800

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -53,7 +53,7 @@ impl Laser {
             match bit == 1 {
                 true => {
                     self.out.set_value(true).expect("Error setting pin");
-                    thread::sleep(Duration::from_micros(10));
+                    thread::sleep(Duration::from_micros(20));
                     self.out.set_value(false).expect("Error setting pin");
                 }
                 false => {
