0fe51f17755975599f11851c77e83f7f8180b579
Paul Kutrich <pkutrich@gmail.com>
2023-11-07 16:41:45 -0800

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -62,7 +62,7 @@ impl Laser {
                     self.out.set_value(false).expect("Error setting pin");
                 }
             }
-            thread::sleep(Duration::from_micros(250))
+            thread::sleep(Duration::from_micros(150))
         }
 
         // Termination sequence.
