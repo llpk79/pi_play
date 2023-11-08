edc08129d4cc9a108042f6f3ebf6887e7ac0783e
Paul Kutrich <pkutrich@gmail.com>
2023-11-08 11:34:53 -0800

    add laser

--- src/lasers.rs
+++ src/lasers.rs
@@ -115,8 +115,8 @@ impl Receiver {
             // println!("bit time {}", bit_time);
             match bit_time {
                 i64::MIN..=-0_i64 => continue,
-                1..=75 => data.push(0),
-                76..=900 => data.push(1),
+                1..=100 => data.push(0),
+                101..=900 => data.push(1),
                 901..=1500 => {
                     continue;
                 }
