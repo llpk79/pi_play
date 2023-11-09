0af331c55d4c4a5faf5101eab05a5a5ec40b3b53
Paul Kutrich <pkutrich@gmail.com>
2023-11-08 21:39:18 -0800

    add laser

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::{fs, thread};
 fn main() {
     let mut laser = Laser::new();
     let mut receiver = Receiver::new();
-    let message = fs::read_to_string("./src/main.rs").expect("error opening file");
+    let message = fs::read_to_string("./src/lasers.rs").expect("error opening file");
 
     let receiver_thread = thread::Builder::new()
         .name("receiver".to_string())
@@ -16,10 +16,7 @@ fn main() {
     let laser_thread = thread::Builder::new()
         .name("laser".to_string())
         .spawn(move || loop {
-            laser.send_message(
-                message.clone(),
-                // "Hello World ".to_string(),
-            );
+            laser.send_message(message.clone());
             thread::sleep(Duration::from_millis(250))
         });
 
