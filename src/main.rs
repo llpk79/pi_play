3cab98400e6b45417d06dd735cc445f4a59aea53
Paul Kutrich <pkutrich@gmail.com>
2023-11-10 23:48:18 -0800

    compression.

--- src/main.rs
+++ src/main.rs
@@ -10,56 +10,24 @@ fn main() {
     // let message = "Hello.".to_string();
 
     // Encode message with Huffman coding.
+    // Use a binary heap to store the frequency of each character.
+    // Not a difficult algorithm. should be easy. go for it.
     let mut heap = BinaryHeap::new();
-    let mut freq_map = HashMap::new();
-    for char in message.chars() {
-        let count = freq_map.entry(char).or_insert(0);
+    let mut freq = HashMap::new();
+    for c in message.chars() {
+        let count = freq.entry(c).or_insert(0);
         *count += 1;
     }
-    for (char, freq) in freq_map {
-        heap.push((freq, char));
+    for (c, f) in freq {
+        heap.push((f, c));
     }
     while heap.len() > 1 {
-        let (freq1, _) = heap.pop().unwrap();
-        let (freq2, _) = heap.pop().unwrap();
-        heap.push((freq1 + freq2, '%'));
+        let (f1, c1) = heap.pop().unwrap();
+        let (f2, c2) = heap.pop().unwrap();
+        heap.push((f1 + f2, '%'));
     }
-    let mut code_map = HashMap::new();
-    let mut code = String::new();
-
-    // Iterate through heap to create code map.
-    let (_, root) = heap.pop().unwrap();
-    fn create_code_map(
-        node: &BinaryHeap<(i32, char)>,
-        code_map: &mut HashMap<char, String>,
-        code: &mut String,
-    ) {
-        match node.peek() {
-            Some((_, '%')) => {
-                code_map.insert('%', code.clone());
-            }
-            Some((_, char)) => {
-                code_map.insert(*char, code.clone());
-            }
-            None => {}
-        }
-        match node.peek() {
-            Some((_, '%')) => {}
-            Some((_, _)) => {
-                code.push('0');
-                create_code_map(&node, code_map, code);
-                code.pop();
-                code.push('1');
-                create_code_map(&node, code_map, code);
-                code.pop();
-            }
-            None => {}
-        }
-    }
-    create_code_map(&heap, &mut code_map, &mut code);
-
-    // Print code map.
-    println!("Code map {:?}", code_map);
+    let mut tree = heap.pop().unwrap();
+    println!("{:?}", tree);
 
 
     let receiver_thread = thread::Builder::new()
