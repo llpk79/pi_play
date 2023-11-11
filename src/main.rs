1fd9bdfcc29f6d441817b3a88c815de8f112b7f1
Paul Kutrich <pkutrich@gmail.com>
2023-11-10 21:15:59 -0800

    compression.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 use pi_play_lib::lasers::{Laser, Receiver};
 use std::time::Duration;
 use std::{fs, thread};
+use std::collections::HashMap;
 
 fn main() {
     let mut laser = Laser::new();
     let mut receiver = Receiver::new();
     let message = fs::read_to_string("./src/temp_humid.rs").expect("error opening file");
     // let message = "Hello.".to_string();
+    // count the unicode characters in the message
+    let mut char_map: HashMap<char, u32> = HashMap::new();
+    for char in message.chars() {
+        let count = char_map.entry(char).or_insert(0);
+        *count += 1;
+    }
+    println!("char_map {:?}", char_map);
 
     let receiver_thread = thread::Builder::new()
         .name("receiver".to_string())
