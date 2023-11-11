1da3356fd4cf3969b9d6a6a3cf8f93e3efb38554
Paul Kutrich <pkutrich@gmail.com>
2023-11-10 22:53:49 -0800

    compression.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,9 @@ fn main() {
     // Create huffman encoding for message utilizing BinaryHeap.
     // Produce a hashmap of codes to chars for decoding.
     let mut heap = BinaryHeap::new();
-    let mut char_counts: HashMap<char, i32> = HashMap::new();
+    let mut char_counts: HashMap<String, i32> = HashMap::new();
     for char in message.chars() {
-        let count = char_counts.entry(char).or_insert(0);
+        let count = char_counts.entry(format!("{}", char)).or_insert(0);
         *count += 1;
     }
     for (char, count) in char_counts {
@@ -22,38 +22,28 @@ fn main() {
     while heap.len() > 1 {
         let (count1, char1) = heap.pop().unwrap();
         let (count2, char2) = heap.pop().unwrap();
-        heap.push((count1 + count2, char1));
-        heap.push((count1 + count2, char2));
+        heap.push((count1 + count2, char1 + &format!(" {}", char2)));
     }
     let mut code_map: HashMap<char, String> = HashMap::new();
     let mut code = String::new();
-    let mut code_stack: Vec<(i32, char)> = Vec::new();
-    let mut code_stack2: Vec<(i32, char)> = Vec::new();
-
-    while let Some((count, char)) = heap.pop() {
-        code_stack.push((count, char));
+    let mut char_stack = Vec::new();
+    let (count, chars) = heap.pop().unwrap();
+    for char in chars.split(" ") {
+        char_stack.push(char);
     }
-    while let Some((count, char)) = code_stack.pop() {
-        code_stack2.push((count, char));
-    }
-    while let Some((count, char)) = code_stack2.pop() {
-        if char.is_alphanumeric() {
-            code_map.insert(char, code.clone());
-        }
-        code.push('0');
-        if count < 0 {
+    while !char_stack.is_empty() {
+        let char = char_stack.pop().unwrap();
+        if char.len() == 1 {
+            code_map.insert(char.chars().next().unwrap(), code.clone());
             code.pop();
+        } else {
+            code.push('0');
+            char_stack.push(&char[1..]);
             code.push('1');
+            char_stack.push(&char[0..1]);
         }
     }
     println!("{:?}", code_map);
-    // Send message.
-    let mut encoded_message = String::new();
-    for char in message.chars() {
-        encoded_message.push_str(&code_map[&char]);
-    }
-    println!("{}", encoded_message);
-
 
     let receiver_thread = thread::Builder::new()
         .name("receiver".to_string())
