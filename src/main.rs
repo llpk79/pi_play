eb9d457a1883731dbaf3fe8c18fb1bd8a854a58a
Paul Kutrich <pkutrich@gmail.com>
2023-11-10 23:15:58 -0800

    compression.

--- src/main.rs
+++ src/main.rs
@@ -9,47 +9,40 @@ fn main() {
     let message = fs::read_to_string("./src/temp_humid.rs").expect("error opening file");
     // let message = "Hello.".to_string();
     // Create huffman encoding for message utilizing BinaryHeap.
-    // Produce a hashmap of codes to chars for decoding.
     let mut heap = BinaryHeap::new();
-    let mut char_counts: HashMap<String, i32> = HashMap::new();
+    let mut freq_map = HashMap::new();
     for char in message.chars() {
-        let count = char_counts.entry(format!("{}", char)).or_insert(0);
+        let count = freq_map.entry(char).or_insert(0);
         *count += 1;
     }
-    for (char, count) in char_counts {
-        heap.push((count, char));
+    for (char, freq) in freq_map {
+        heap.push((freq, char));
     }
     while heap.len() > 1 {
-        let (count1, char1) = heap.pop().unwrap();
-        let (count2, char2) = heap.pop().unwrap();
-        heap.push((count1 + count2, char1 + &format!(" {}", char2)));
+        let (freq1, _) = heap.pop().unwrap();
+        let (freq2, _) = heap.pop().unwrap();
+        heap.push((freq1 + freq2, '%'));
     }
+    let mut encoding_map = HashMap::new();
 
-    let mut code_map: HashMap<String, String> = HashMap::new();
-    let mut code = String::new();
-    let mut code_stack: Vec<String> = Vec::new();
-    let mut char_stack: Vec<String> = Vec::new();
-    let (count, chars) = heap.pop().unwrap();
-    code_stack.push(code);
-    char_stack.push(chars);
-    while !char_stack.is_empty() {
-        let chars = char_stack.pop().unwrap();
-        let code = code_stack.pop().unwrap();
-        if chars.len() == 1 {
-            code_map.insert(chars, code);
+    // traverse tree to create encoding map.
+    let heap_vec = heap.into_vec();
+    let mut tree = heap_vec[0].1.to_string();
+    let mut encoding = String::new();
+    while tree.len() > 0 {
+        let char = tree.remove(0);
+        if char == '%' {
+            encoding.pop();
+            let char = tree.remove(0);
+            encoding_map.insert(char, encoding.clone());
+            encoding.pop();
         } else {
-            let mut chars = chars.split(" ").collect::<Vec<_>>();
-            let char1 = chars.pop().unwrap();
-            let char2 = chars.pop().unwrap();
-            let code1 = code.clone() + "0";
-            let code2 = code + "1";
-            code_stack.push(code1);
-            code_stack.push(code2);
-            char_stack.push(char1.to_string());
-            char_stack.push(char2.to_string());
+            encoding.push('0');
+            tree.push(char);
+            tree.push('1');
         }
     }
-    println!("{:?}", code_map);
+    println!("{:?}", encoding_map);
 
 
     let receiver_thread = thread::Builder::new()
