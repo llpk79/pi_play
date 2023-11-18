7e3a5825b90167dfec7061e5fd783fcf98ff5d4e
Paul Kutrich <pkutrich@gmail.com>
2023-11-17 21:53:37 -0800

    move do_laser to main. fmt.

--- src/lasers.rs
+++ src/lasers.rs
@@ -191,7 +191,7 @@ impl Receiver {
         let seconds = (end - start).num_milliseconds() as f64 / 1000.0_f64;
         let decode_time = (chrono::Utc::now() - end).num_milliseconds() as f64 / 1000.0_f64;
         println!(
-            "Message in {:.3} sec\nDecode time {:.3} sec\nKB/s {:.3}\n'Error' {:.5}\n",
+            "Message in {:.3} sec\nDecode time {:.5} sec\nKB/s {:.3}\n'Error' {:.5}\n",
             seconds,
             decode_time,
             num_kbytes / seconds,
