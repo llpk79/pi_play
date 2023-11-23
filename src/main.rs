00a8511dab4b37b9ea2b020a1affc7b6b0455ac9
Paul Kutrich <pkutrich@gmail.com>
2023-11-22 23:09:32 -0800

    add dot matrix display

--- src/main.rs
+++ src/main.rs
@@ -84,9 +84,9 @@ fn do_laser() {
             }
 
             dot_matrix.display_data(&dot_matrix_data.data[6], dot_matrix_data.tab);
-            prev_humidity = if humidity > prev_humidity {humidity} else { prev_humidity };
-            prev_temp = if celsius > prev_temp {celsius} else {prev_temp};
-            prev_pressure = if pressure > prev_pressure {pressure} else { prev_pressure };
+            prev_humidity = if humidity != prev_humidity {humidity} else { prev_humidity };
+            prev_temp = if celsius != prev_temp {celsius} else {prev_temp};
+            prev_pressure = if pressure != prev_pressure {pressure} else { prev_pressure };
 
             thread::sleep(Duration::from_millis(30_000))
         });
