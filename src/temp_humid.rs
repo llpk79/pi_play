0a770650a7633b00dd452cc17c0ef1c732dfa669
Paul Kutrich <pkutrich@gmail.com>
2023-12-06 16:30:45 -0800

    bit timing.

--- src/temp_humid.rs
+++ src/temp_humid.rs
@@ -29,7 +29,7 @@ pub fn measure_temp_humid() -> (f32, f32) {
         let start = chrono::Utc::now();
         let mut limit = 0;
         while data_pin.read_value().unwrap() == High {
-            if limit > 35 {
+            if limit > 30 {
                 // println!("bit hung");
                 break;
             } else {
