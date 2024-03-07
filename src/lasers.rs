7b57ec030ab011e410c561ed8d1e1fbe2ef0fc7c
Paul Kutrich <pkutrich@gmail.com>
2024-03-07 00:38:39 -0800

    Add do_lasers

--- src/lasers.rs
+++ src/lasers.rs
@@ -154,3 +154,39 @@ impl Receiver {
         );
     }
 }
+
+/// Send a message with a laser!
+pub fn do_laser(message: String) {
+    // Compress message with Huffman Coding.
+    let mut huff_tree = HuffTree::new();
+    let encoded_message = huff_tree.encode(message);
+
+    // Pass huff_tree to receiver to decode message.
+    let mut receiver = Receiver::new(huff_tree);
+    let mut laser = Laser::new(encoded_message);
+
+    // Start a thread each for the laser and receiver.
+    let receiver_thread = thread::Builder::new()
+        .name("receiver".to_string())
+        .spawn(move || loop {
+            receiver.print_message();
+        });
+
+    let laser_thread = thread::Builder::new()
+        .name("laser".to_string())
+        .spawn(move || loop {
+            laser.send_message();
+            thread::sleep(Duration::from_millis(2000))
+        });
+
+    receiver_thread
+        .expect("Thread should exist")
+        .join()
+        .expect("Thread should close");
+    laser_thread
+        .expect("Thread should exist")
+        .join()
+        .expect("Thread should close");
+}
+
+
